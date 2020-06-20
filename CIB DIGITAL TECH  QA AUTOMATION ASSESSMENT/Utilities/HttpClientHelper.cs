e080953b526e709a9879d7f06ff3282bbc394da6
Ephraim <ephraiml@eohmc.onmicrosoft.com>
2020-06-20 15:19:38 +0200

    Api Test class HttpHelper Class
    Add user page objects
    User table page objects
    Web Task 2 test class

---
+++ CIB DIGITAL TECH  QA AUTOMATION ASSESSMENT/Utilities/HttpClientHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIB_DIGITAL_TECH__QA_AUTOMATION_ASSESSMENT.Utilities
+{
+    class HttpClientHelper
+    {
+    }
+}
---
+++ CIB DIGITAL TECH  QA AUTOMATION ASSESSMENT/WebAutomation/TestExecution/ExecuteTask2TestClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIB_DIGITAL_TECH__QA_AUTOMATION_ASSESSMENT.WebAutomation.TestExecution
+{
+    class ExecuteTask2TestClass
+    {
+    }
+}
